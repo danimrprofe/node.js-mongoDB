dc1ca4d68198b90ff29970751d933d1c290abaad
danimrprofe <danimrprofe@gmail.com>
2019-04-22 11:50:29 +0200

    migrado de express 3 a 4

--- server/configure.js
+++ server/configure.js
@@ -1,12 +1,16 @@
 // Definición del módulo configure
 // Declarando veriables para todos los módulos que vamos a utilizar
 
-var connect = require('connect');
+var express = require('express');
+
 var path = require('path');
 // routes = require('./routes');
 var exphbs = require('express-handlebars');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
 
 // Definimos el módulo que vamos a crear y exportar
 // Le pasamos el objeto app como parámetro, lo modifica y lo devuelve
@@ -32,19 +36,21 @@ module.exports = function (app) {
   // app.use(connect.logger('dev')); // Logea cada petición HTTP recibida
   // Ahora hay que logear utilizando un módulo llamado Morgan, tócatelos
 
-  app.use(logger('combined'));
-
-  app.use(connect.bodyParser({
+  app.use(bodyParser({
     uploadDir: path.join(__dirname, '../public/upload/temp')
   }));
-  app.use(connect.json());
-  app.use(connect.urlencoded());
-  app.use(connect.methodOverride());
-  app.use(connect.cookieParser('some-secret-value-here'));
-  app.use(app.router);
-  app.use('/public/', connect.static(path.join(__dirname, '../public'))); // Para servir contenido estático
-  if (app.get('env') === 'development') {
-    app.use(connect.errorHandler());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+  app.use(logger('combined'));
+  // app.use(multer());
+  app.use(methodOverride());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Ni flowers de como cambiar esto: app.use(connect.cookieParser('some-secret-value-here'));
+
+  // error handling middleware should be loaded after the loading the routes
+  if (app.get('env') == 'development') {
+    app.use(errorHandler());
   }
 
   // Hasta aquí todo el festival de middleware que necesitaremos
